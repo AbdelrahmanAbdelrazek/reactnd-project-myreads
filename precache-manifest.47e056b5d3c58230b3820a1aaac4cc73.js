fbdbe54972e74d5c4827c8816385824febd13393
Sarah Maris <sarah@sarahmaris.com>
2019-05-22 22:23:55 -0400

    Updates

---
+++ precache-manifest.47e056b5d3c58230b3820a1aaac4cc73.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44587a8de2bce7269670b56a7bf515bd",
+    "url": "/reactnd-project-myreads/index.html"
+  },
+  {
+    "revision": "c51b65d4bc9e2cc84378",
+    "url": "/reactnd-project-myreads/static/css/main.f08ccb1f.chunk.css"
+  },
+  {
+    "revision": "e316499cc4928ca340d4",
+    "url": "/reactnd-project-myreads/static/js/2.31e400a7.chunk.js"
+  },
+  {
+    "revision": "c51b65d4bc9e2cc84378",
+    "url": "/reactnd-project-myreads/static/js/main.f9079fdb.chunk.js"
+  },
+  {
+    "revision": "52f022a7f7eee91acd3c",
+    "url": "/reactnd-project-myreads/static/js/runtime~main.7192d003.js"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/reactnd-project-myreads/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/reactnd-project-myreads/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/reactnd-project-myreads/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "51adb8eb8d23044d1807e76fb6dda947",
+    "url": "/reactnd-project-myreads/static/media/stacked-books.51adb8eb.jpg"
+  }
+]);
